b8934d557b773fa2696604f0e08ae549ad79b975
Ephy Kamau <79284405+7961-Ephy@users.noreply.github.com>
2023-02-16 01:19:02 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import Heading from "./Heading";
+import Note from "./Note";
+import Footer from "./Footer";
+
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
